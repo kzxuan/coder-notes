b7de0cd62ab2b9ad77bb5261c5321930224071b5
KzXuan <kzxuan@qq.com>
2021-08-26 14:29:52 +0800

    [init] Initial project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/coder-notes/',
+  lang: 'zh-CN',
+  title: 'Coder Notes',
+  description: 'Coder notes.',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+
+  themeConfig: {
+    logo: '/logo.png',
+  },
+}
